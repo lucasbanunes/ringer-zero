8fca2d518be609682e7c7b627010422cdf433f2f
lucasbanunes <lucasbanunes@gmail.com>
2026-03-22 20:10:26 -0300

    Adds vqat to the cli

--- cli.py
+++ cli.py
@@ -1,8 +1,10 @@
 import typer
 from ringer_zero.datasets import app as dataset_app
+from ringer_zero.models.vqat import app as vqat_app
 
 
 app = typer.Typer()
+app.add_typer(vqat_app, name='vqat')
 app.add_typer(dataset_app, name='datasets')
 
 if __name__ == '__main__':
