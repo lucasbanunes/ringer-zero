721edb1ee0535eb943ce4c87cb91450225d4f3bf
lucasbanunes <lucasbanunes@gmail.com>
2026-03-27 08:14:18 -0300

    Updates setup.sh with the correct root version

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-conda create -c conda-forge --name ringer-zero root python=3.12
+conda create -c conda-forge --name ringer-zero root==6.36.06 python==3.12.12
 conda run -n ringer-zero pip install -r requirements.txt
