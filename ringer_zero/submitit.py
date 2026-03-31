8973e5f282c8c273044a812b05551a87e8b73f92
Pedro Lima <pedrohglima01@gmail.com>
2026-03-31 16:09:01 -0300

    Minor style changes
    Change formatation, no changes in behaviour

--- ringer_zero/submitit.py
+++ ringer_zero/submitit.py
@@ -35,58 +35,38 @@ class ExecutorConfig(BaseModel):
     """
 
     cpus_per_task: Annotated[
-        int,
-        Field(
-            description='Number of CPUs to allocate per task'
-        )
+        int, Field(description="Number of CPUs to allocate per task")
     ]
     executor_type: Annotated[
-        Literal['slurm', 'debug', 'local'],
+        Literal["slurm", "debug", "local"],
         Field(
             description='Type of executor to use. "slurm" submits the jobs to a slurm cluster, "debug" runs the jobs directly in the current process and "local" runs the jobs in parallel in the local machine using submitit LocalExecutor.'
-        )
+        ),
     ]
     logs_dir: Annotated[
-        Path,
-        Field(
-            description='Directory to save the logs of the slurm jobs'
-        )
+        Path, Field(description="Directory to save the logs of the slurm jobs")
     ]
-    name: Annotated[
-        str,
-        Field(
-            description='Name of the job'
-        )
-    ]
+    name: Annotated[str, Field(description="Name of the job")]
     slurm_array_parallelism: Annotated[
         int,
-        Field(
-            description='Max number of jobs to run in parallel in the slurm array'
-        )
+        Field(description="Max number of jobs to run in parallel in the slurm array"),
     ] = 1
     slurm_partition: Annotated[
-        str | None,
-        Field(
-            description='Slurm partition to submit the jobs to'
-        )
+        str | None, Field(description="Slurm partition to submit the jobs to")
     ] = None
     stderr_to_stdout: Annotated[
         bool,
-        Field(
-            description='Whether to redirect stderr to stdout in the slurm jobs'
-        )
+        Field(description="Whether to redirect stderr to stdout in the slurm jobs"),
     ] = True
     timeout_min: Annotated[
-        int,
-        Field(
-            description='Timeout in minutes for the slurm jobs'
-        )
-    ] = 24*60  # 1 day
+        int, Field(description="Timeout in minutes for the slurm jobs")
+    ] = 24 * 60  # 1 day
 
     def model_post_init(self, context):
-        if self.executor_type == 'slurm' and self.slurm_partition is None:
+        if self.executor_type == "slurm" and self.slurm_partition is None:
             raise ValueError(
-                'slurm_partition must be specified when using slurm executor')
+                "slurm_partition must be specified when using slurm executor"
+            )
         return super().model_post_init(context)
 
     def update_executor_params(self, executor: submitit.Executor) -> submitit.Executor:
@@ -96,22 +76,21 @@ class ExecutorConfig(BaseModel):
             slurm_array_parallelism=self.slurm_array_parallelism,
             slurm_partition=self.slurm_partition,
             stderr_to_stdout=self.stderr_to_stdout,
-            timeout_min=self.timeout_min
+            timeout_min=self.timeout_min,
         )
         return executor
 
     def get_executor(self) -> submitit.Executor:
         match self.executor_type:
-            case 'slurm':
+            case "slurm":
                 executor = submitit.AutoExecutor(folder=str(self.logs_dir))
-            case 'debug':
+            case "debug":
                 # Usual submitit executors don't work in debug mode, so we use a custom executor that just runs the function directly.
                 executor = DebugExecutor(folder=str(self.logs_dir))
-            case 'local':
+            case "local":
                 executor = submitit.LocalExecutor(folder=str(self.logs_dir))
             case _:
-                raise ValueError(
-                    f'Invalid executor type: {self.executor_type}')
+                raise ValueError(f"Invalid executor type: {self.executor_type}")
 
         executor = self.update_executor_params(executor)
         return executor
--- tests/test_models/test_vqat.py
+++ tests/test_models/test_vqat.py
@@ -8,37 +8,23 @@ from ringer_zero.submitit import ExecutorConfig
 
 def test_vqat_training_job(test_data_dir: Path):
     with TemporaryDirectory() as output_dir:
-        logging.info(f'Saving results to {output_dir}')
+        logging.info(f"Saving results to {output_dir}")
         dataset_dir = test_data_dir / "test_dataset"
         output_dir = Path(output_dir)
 
         job = VQATTrainingJob(
             dataset_dir=dataset_dir,
-            data_table='data',
-            rings_col='trig_L2_calo_rings',
-            kfold_table='standard_binning_kfold',
-            label_col='label',
-            fold_col='fold',
-            et_col='trig_L2_calo_et',
-            et_bins=(
-                15000.0,
-                20000.0,
-                30000.0,
-                40000.0,
-                50000.0,
-                float('inf')
-            ),
-            eta_col='trig_L2_calo_eta',
-            eta_bins=(
-                0.0,
-                0.8,
-                1.37,
-                1.54,
-                2.37,
-                2.5
-            ),
+            data_table="data",
+            rings_col="trig_L2_calo_rings",
+            kfold_table="standard_binning_kfold",
+            label_col="label",
+            fold_col="fold",
+            et_col="trig_L2_calo_et",
+            et_bins=(15000.0, 20000.0, 30000.0, 40000.0, 50000.0, float("inf")),
+            eta_col="trig_L2_calo_eta",
+            eta_bins=(0.0, 0.8, 1.37, 1.54, 2.37, 2.5),
             output_dir=output_dir,
-            tag='vqat',
+            tag="vqat",
             b0=22,
             i0=7,
             batch_size=32,
@@ -46,29 +32,29 @@ def test_vqat_training_job(test_data_dir: Path):
             dry_run=True,
             executor_config=ExecutorConfig(
                 cpus_per_task=1,
-                executor_type='debug',
-                logs_dir='./logs',
-                name='test_vqat_training_job',
+                executor_type="debug",
+                logs_dir="./logs",
+                name="test_vqat_training_job",
                 slurm_array_parallelism=1,
                 slurm_partition=None,
                 stderr_to_stdout=True,
-                timeout_min=5
-            )
+                timeout_min=5,
+            ),
         )
         job.run()
-        logging.info(f'Output files: {list(output_dir.glob("*"))}')
+        logging.info(f"Output files: {list(output_dir.glob('*'))}")
 
 
 def test_vqat_training_job_from_yaml(test_data_dir: Path):
     with TemporaryDirectory() as output_dir:
-        logging.info(f'Saving results to {output_dir}')
+        logging.info(f"Saving results to {output_dir}")
         dataset_dir = test_data_dir / "test_dataset"
         output_dir = Path(output_dir)
 
         job = VQATTrainingJob.from_yaml(
             test_data_dir / "vqat_training_job.yaml",
             output_dir=output_dir,
-            dataset_dir=dataset_dir
+            dataset_dir=dataset_dir,
         )
         job.run()
-        logging.info(f'Output files: {list(output_dir.glob("*"))}')
+        logging.info(f"Output files: {list(output_dir.glob('*'))}")
