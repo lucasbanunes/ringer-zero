2ddfbb653eb16b2f0c7383fec7dd0329eaebc5e8
lucasbanunes <lucasbanunes@gmail.com>
2026-04-05 22:22:05 -0300

    Moves notebooks to ringer-datasets and updates setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-conda create -c conda-forge --name ringer-zero root==6.36.06 python==3.12.12
-conda run -n ringer-zero pip install -r requirements.txt
+conda config --set channel_priority strict && \
+conda create -c conda-forge --name ringer-zero root==6.36.06 python==3.12.12 && \
+conda run --live-stream -n ringer-zero pip install -r requirements.txt
