a16da1003f688c28242dde539605660c36ce24f2
lucasbanunes <lucasbanunes@gmail.com>
2026-04-09 08:51:25 -0300

    Adds mlp subcommands to cli.py

--- cli.py
+++ cli.py
@@ -1,14 +1,17 @@
 import typer
 from ringer_zero.datasets import app as dataset_app
 from ringer_zero.models.vqat import app as vqat_app
+from ringer_zero.models.mlp import app as mlp_app
 
 
 app = typer.Typer(
     help='Ringer Zero CLI',
     rich_markup_mode="markdown"
 )
-app.add_typer(vqat_app, name='vqat')
 app.add_typer(dataset_app, name='datasets')
+app.add_typer(mlp_app, name='mlp')
+app.add_typer(vqat_app, name='vqat')
+
 
 if __name__ == '__main__':
     app()
